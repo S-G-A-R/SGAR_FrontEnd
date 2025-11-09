6ec4a8ab9883cd0e1746c28e98173be4e9668a4e
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-11-09 17:37:27 -0600

    Se programo el service, la clase credencialesrequest y se configuro el program

---
+++ FrontEnd_SGAR/Models/CredencialesRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace FrontEnd_SGAR.Models
+{
+    public class CredencialesRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
--- FrontEnd_SGAR/Program.cs
+++ FrontEnd_SGAR/Program.cs
@@ -1,4 +1,5 @@
 using FrontEnd_SGAR;
+using FrontEnd_SGAR.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri("http://sgarseguridad.somee.com/") //API SEGURIDAD
+});
+
+//Servicio de autenticacion
+builder.Services.AddScoped<AuthSeguridadService>();
+
 
 await builder.Build().RunAsync();
