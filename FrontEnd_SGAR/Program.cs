e3184faed9901ca4332ccd76ee8052c819a575cc
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-11-09 21:45:23 -0600

    Se eliminaron archivos

--- FrontEnd_SGAR/Program.cs
+++ FrontEnd_SGAR/Program.cs
@@ -13,17 +13,10 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri("http://sgarseguridad.somee.com/") //API SEGURIDAD
 });
 
-// CONEXIÓN DE LA API DE NAVEGACIÓN
-builder.Services.AddHttpClient("NavigationAPI", client =>
-{
-    client.BaseAddress = new Uri("https://sgar-navigation.vercel.app/");
-});
 
 //Servicio de autenticacion
 builder.Services.AddScoped<AuthSeguridadService>();
 builder.Services.AddScoped<AuthService>();
-
-builder.Services.AddScoped<MunicipioService>();
 
 
 await builder.Build().RunAsync();
