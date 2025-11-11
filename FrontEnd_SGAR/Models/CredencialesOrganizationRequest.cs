fc5e6ffd1bf2bc23c736eef7714afabb24e9dabd
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-11-11 09:23:41 -0600

    Se les coloco validaciones a los campos de email y password de los login

--- FrontEnd_SGAR/Models/CredencialesOrganizationRequest.cs
+++ FrontEnd_SGAR/Models/CredencialesOrganizationRequest.cs
@@ -1,8 +1,16 @@
-﻿namespace FrontEnd_SGAR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd_SGAR.Models
 {
     public class CredencialesOrganizationRequest
     {
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Debe ingresar un correo válido.")]
         public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [StringLength(8, MinimumLength = 5, ErrorMessage = "La contraseña debe tener entre 5 y 8 caracteres.")]
+        [RegularExpression(@"^(?!.*-\d).*", ErrorMessage = "La contraseña no puede contener números negativos.")]
         public string Password { get; set; } = string.Empty;
     }
 }
