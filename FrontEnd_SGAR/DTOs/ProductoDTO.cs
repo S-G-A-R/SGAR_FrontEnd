8bbdf5db4e61d57be90777205a95c6082c4abc67
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-11-25 18:17:57 -0600

    Se avanzo en la vista de crear producto

---
+++ FrontEnd_SGAR/DTOs/ProductoDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FrontEnd_SGAR.DTOs
+{
+    public class ProductoDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public int CategoriaProductoId { get; set; }
+        public int EmpresaId { get; set; }
+        public string ImagenBase64 { get; set; } = string.Empty;
+    }
+}
