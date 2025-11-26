629371f6cb5dc5811dd849c8fc9438ba3271c7d6
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-11-26 17:03:38 -0600

    Se trabajo en la vista de administrar usuarios

--- FrontEnd_SGAR/Services/PlanService.cs
+++ FrontEnd_SGAR/Services/PlanService.cs
@@ -96,33 +96,5 @@ namespace FrontEnd_SGAR.Services
         {
             public string? message { get; set; }
         }
-
-        private class PlanResponsePaginada
-        {
-            [JsonPropertyName("totalItems")] public int TotalItems { get; set; }
-            [JsonPropertyName("totalPages")] public int TotalPages { get; set; }
-            [JsonPropertyName("pageSize")] public int PageSize { get; set; }
-            [JsonPropertyName("currentPage")] public int CurrentPage { get; set; }
-            [JsonPropertyName("planes")] public List<PlanDetalleDTO>? Planes { get; set; }
-        }
-
-        public class PlanDetalleDTO
-        {
-            [JsonPropertyName("id")] public int id { get; set; }
-            [JsonPropertyName("asociadoId")] public int asociadoId { get; set; }
-            [JsonPropertyName("tipoSuscripcion")] public TipoSuscripcionDTO? tipoSuscripcion { get; set; }
-            [JsonPropertyName("fechaInicio")] public DateTime fechaInicio { get; set; }
-            [JsonPropertyName("fechaFin")] public DateTime fechaFin { get; set; }
-            [JsonPropertyName("activo")] public bool activo { get; set; }
-        }
-
-        public class PlanVerificacionResult
-        {
-            public bool TienePlanActivo { get; set; }
-            public int DiasRestantes { get; set; }
-            public PlanDetalleDTO? Plan { get; set; }
-            public string? Mensaje { get; set; }
-        }
-
     }
 }
