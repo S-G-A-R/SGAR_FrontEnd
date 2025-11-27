03f6bb5db95dae5d4a5439fb865bdefeaa130bde
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-11-27 08:26:35 -0600

    Mejoras de Asignacion de Horario a operador

---
+++ FrontEnd_SGAR/DTOs/OperadorDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FrontEnd_SGAR.DTOs
+{
+    public class OperadorDTO
+    {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public string CodigoOperador { get; set; } = "";
+        public string LicenciaDoc { get; set; } = "";
+        public int IdOrganizacion { get; set; }
+    }
+}
---
+++ FrontEnd_SGAR/DTOs/OperadorHorarioDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FrontEnd_SGAR.DTOs
+{
+    public class OperadorHorarioDTO
+    {
+        public int Id { get; set; }
+
+        public int OperadorId { get; set; }
+        public string CodigoOperador { get; set; } = ""; // viene de operador
+
+        public List<HorarioDTO> Horarios { get; set; } = new();
+        public List<int> HorariosIds { get; set; } = new(); // para POST/PUT
+    }
+}
